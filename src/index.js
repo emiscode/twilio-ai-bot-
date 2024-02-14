bcd6aed7706e7947d882f96e4877f59a1153dd89
Emilio Scodeler <emiscode@gmail.com>
2024-02-14 15:56:02 -0300

    add open ai

--- src/index.js
+++ src/index.js
@@ -1,15 +1,262 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const twilio = require("twilio");
+const OpenAI = require("openai");
 
 // Setup express app
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Setup twilio client
 const VoiceResponse = twilio.twiml.VoiceResponse;
 
+// Setup OpenAI client
+const apiKey = process.env.OPENAI_API_KEY;
+const organization = process.env.OPENAI_ORGANIZATION;
+const openai = new OpenAI({ apiKey, organization });
+
+// Store conversation context for each client
+let conversation_contexts = {};
+
+app.get("/food", async (_req, res) => {
+  // Generate a response using OpenAI's GPT-4 model
+  const openaiResponse = await openai.chat.completions.create({
+    model: "gpt-4-turbo-preview",
+    max_tokens: 2000,
+    messages: [
+      {
+        role: "system",
+        content: `You are a helpful assistant for ordering food on a restaurant.
+          You can take orders, answer questions, and provide recommendations.
+          Make the response short and clear.
+          When the user confirms the order then your answer should contain a summary of the order,
+          and at the end a token to indicate the end of the conversation, like this #order_confirmed#.`,
+      },
+      {
+        role: "user",
+        content:
+          "Hi, what do you recommend for dinner? I'm in the mood for something spicy.",
+      },
+    ],
+    stream: true,
+  });
+
+  // Set the headers to enable SSE
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  let chunks = [];
+  let buffer = "";
+
+  // Send the AI's response to the client as streaming data
+  for await (const chunk of openaiResponse) {
+    const content = chunk.choices[0]?.delta?.content || "";
+    buffer += content;
+
+    if (buffer.includes(".")) {
+      const currentBuffer = buffer.split(".")[0] + ".";
+      buffer = buffer.split(".")[1];
+      chunks.push(currentBuffer);
+
+      console.log({ currentBuffer, buffer });
+      res.write(currentBuffer);
+    }
+  }
+
+  console.log("chunks:", chunks.join(" "));
+
+  // End the response
+  res.end();
+});
+
+app.post("/test", async (req, res) => {
+  console.log("called test process_speech");
+
+  // The transcribed text is in the 'SpeechResult' parameter
+  const { body } = req;
+
+  const speechResult = body.text;
+  console.log("The user said:", speechResult);
+
+  // Get the client's phone number
+  const phoneNumber = body.phone;
+  console.log("The client's phone number is:", phoneNumber);
+
+  // If this is the first time this client is calling, initialize their conversation context
+  if (!conversation_contexts[phoneNumber]) {
+    console.log("initialize conversation context");
+
+    conversation_contexts[phoneNumber] = [
+      {
+        role: "system",
+        content: `You are a helpful assistant for ordering food on a restaurant.
+          You can take orders, answer questions, and provide recommendations.
+          Make the response short and clear.
+          When the user confirms the order then your answer should contain a summary of the order,
+          and at the end a token to indicate the end of the conversation, like this #order_confirmed#.`,
+      },
+    ];
+  }
+
+  console.log("after init conversation_contexts:", conversation_contexts);
+
+  // Add the user's input to the conversation context
+  conversation_contexts[phoneNumber].push({
+    role: "user",
+    content: speechResult,
+  });
+
+  console.log(
+    "after Add the user's input to the conversation context:",
+    conversation_contexts
+  );
+
+  console.log("before openaiResponse: ", conversation_contexts[phoneNumber]);
+
+  // Generate a response using OpenAI's GPT-4 model
+  const openaiResponse = await openai.chat.completions.create({
+    model: "gpt-4-turbo-preview",
+    max_tokens: 2000,
+    messages: conversation_contexts[phoneNumber],
+  });
+
+  // Get the AI's response
+  const aiResponse = openaiResponse["choices"][0]["message"]["content"];
+
+  // Add the AI's response to the conversation context
+  conversation_contexts[phoneNumber].push({
+    role: "assistant",
+    content: aiResponse,
+  });
+
+  console.log("after openaiResponse: ", conversation_contexts[phoneNumber]);
+
+  // Respond to the user with the AI's response
+  res.send(conversation_contexts[phoneNumber]);
+});
+
 app.post("/answer", (req, res) => {
+  console.log("called answer");
+  const twiml = new VoiceResponse({ voice: "Polly.Matthew-Neural" });
+  let counter = parseInt(req.query.counter, 10) || 0;
+  const continueConversation = req.query.continueConversation;
+
+  // Increment the counter since we're attempting again
+  counter++;
+
+  // Decide to gather or hangup based on the counter
+  if (counter <= 3) {
+    // Use <Gather> to collect user input or say something to the user
+    const gather = twiml.gather({
+      input: "speech",
+      action: "/process_speech",
+      method: "POST",
+      timeout: 5,
+      speechTimeout: "auto",
+    });
+
+    // say something to the user
+    const msg = continueConversation
+      ? "Are you still there?"
+      : "Hello! How can I help you today?";
+
+    gather.say(msg);
+
+    // If the user doesn't say anything, loop back to the beginning
+    twiml.redirect(
+      `/answer?counter=${counter}&continueConversation=${continueConversation}`
+    );
+  } else {
+    // Hang up after 3 attempts
+    twiml.say("No answer from the caller. Goodbye!");
+    twiml.hangup();
+  }
+
+  res.type("text/xml");
+  res.send(twiml.toString());
+});
+
+app.post("/process_speech", async (req, res) => {
+  console.log("called process_speech");
+  const twiml = new VoiceResponse({ voice: "Polly.Matthew-Neural" });
+
+  // The transcribed text is in the 'SpeechResult' parameter
+  const speechResult = req.body.SpeechResult;
+  console.log("The user said:", speechResult);
+
+  // Get the client's phone number
+  const phoneNumber = req.body.From;
+  console.log("The client's phone number is:", phoneNumber);
+
+  // If this is the first time this client is calling, initialize their conversation context
+  if (!conversation_contexts[phoneNumber]) {
+    conversation_contexts[phoneNumber] = [
+      {
+        role: "system",
+        content: `You are a helpful assistant for ordering food on a restaurant.
+        You can take orders, answer questions, and provide recommendations.
+        Make the response short and clear.
+        When the client confirms the order then your answer should contains a summary of the order,
+        and at the end a token to indicate the end of the conversation, like this #order_confirmed#.`,
+      },
+    ];
+  }
+
+  // Add the user's input to the conversation context
+  conversation_contexts[phoneNumber].push({
+    role: "user",
+    content: speechResult,
+  });
+
+  // Generate a response using OpenAI's GPT-4 model
+  const openaiResponse = await openai.chat.completions.create({
+    model: "gpt-4-turbo-preview",
+    messages: conversation_contexts[phoneNumber],
+  });
+
+  // Get the AI's response
+  const aiResponse = openaiResponse["choices"][0]["message"]["content"];
+
+  // Add the AI's response to the conversation context
+  conversation_contexts[phoneNumber].push({
+    role: "assistant",
+    content: aiResponse,
+  });
+
+  console.log({ aiResponse });
+
+  // Respond to the user with the AI's response
+  twiml.say(aiResponse.split("#order_confirmed#")[0]);
+
+  // Check if the user has confirmed their order
+  if (aiResponse.includes("#order_confirmed#")) {
+    // Add a pause for a few seconds before ending the call
+    twiml.pause({ length: 1 });
+
+    // Say goodbye and then end the call
+    twiml.say("Thank you for using our service. Goodbye!");
+    twiml.hangup();
+  } else {
+    // Use <Gather> to collect user input or say something to the user
+    twiml.gather({
+      input: "speech",
+      action: "/process_speech",
+      method: "POST",
+      timeout: 5,
+      speechTimeout: "auto",
+    });
+
+    // If the user doesn't say anything, redirect back to /answer
+    twiml.redirect(`/answer?continueConversation=true`);
+  }
+
+  res.type("text/xml");
+  res.send(twiml.toString());
+});
+
+app.post("/old_answer", (req, res) => {
   console.log("called answer");
   const twiml = new VoiceResponse({ voice: "Polly.Matthew-Neural" });
   let counter = parseInt(req.query.counter, 10) || 0;
@@ -43,7 +290,7 @@ app.post("/answer", (req, res) => {
   res.send(twiml.toString());
 });
 
-app.post("/process_speech", async (req, res) => {
+app.post("/old_process_speech", async (req, res) => {
   console.log("called process_speech");
   const twiml = new VoiceResponse({ voice: "Polly.Matthew-Neural" });
 
@@ -51,8 +298,41 @@ app.post("/process_speech", async (req, res) => {
   const speechResult = req.body.SpeechResult;
   console.log("The user said:", speechResult);
 
-  // Assuming you've processed the speech-to-text result and obtained an AI response
-  const aiResponse = "Your order will be delivered in 30 minutes.";
+  // Get the client's phone number
+  const phoneNumber = req.body.From;
+  console.log("The client's phone number is:", phoneNumber);
+
+  // If this is the first time this client is calling, initialize their conversation context
+  if (!conversation_contexts[phoneNumber]) {
+    conversation_contexts[phoneNumber] = [
+      {
+        role: "system",
+        content:
+          "You are a helpful assistant for ordering food on a restaurant. You can take orders, answer questions, and provide recommendations. Make the response short and clear.",
+      },
+    ];
+  }
+
+  // Add the user's input to the conversation context
+  conversation_contexts[phoneNumber].push({
+    role: "user",
+    content: speechResult,
+  });
+
+  // Generate a response using OpenAI's GPT-4 model
+  const openaiResponse = await openai.chat.completions.create({
+    model: "gpt-4-turbo-preview",
+    messages: conversation_contexts[phoneNumber],
+  });
+
+  // Get the AI's response
+  const aiResponse = openaiResponse["choices"][0]["message"]["content"];
+
+  // Add the AI's response to the conversation context
+  conversation_contexts[phoneNumber].push({
+    role: "assistant",
+    content: aiResponse,
+  });
 
   // Respond to the user with the AI's response
   twiml.say(aiResponse);
